13b47cbe10046749bc5274add2e2f10f27802f31
TheBirdCanFly <misha.10myhajlo@gmail.com>
2021-01-16 11:47:16 +0200

    second commit

--- app/src/main/java/com/example/astamobiproject/MainActivity.kt
+++ app/src/main/java/com/example/astamobiproject/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        зразок  2
     }
 
     override fun onRestart() {
