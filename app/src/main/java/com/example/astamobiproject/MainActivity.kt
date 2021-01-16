af6187df6ce05c0349a99884c1b1f13e5a65e54f
TheBirdCanFly <misha.10myhajlo@gmail.com>
2021-01-16 11:33:04 +0200

    1 зборка

---
+++ app/src/main/java/com/example/astamobiproject/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.astamobiproject
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+}
